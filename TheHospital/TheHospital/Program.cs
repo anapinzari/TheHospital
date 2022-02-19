3f5aa3581ce2588d0a0bd93dbcf91c3cb037e082
Ana <ana.pinzari@icloud.com>
2022-02-19 23:43:21 +0200

    Initial commit

---
+++ TheHospital/TheHospital/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheHospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
